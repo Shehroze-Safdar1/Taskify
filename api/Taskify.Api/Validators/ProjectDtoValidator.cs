e89fc891fc0aa34ec041870aa58ad1bd7882abe9
Shehroze-Safdar1 <shehroze.safdar@gmail.com>
2025-09-23 12:16:19 +0500

    Added FluentValidation and Project and Task Management API Handling

---
+++ api/Taskify.Api/Validators/ProjectDtoValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using Taskify.Api.Dtos;
+
+
+namespace Taskify.Api.Validators
+{
+    public class ProjectDtoValidator : AbstractValidator<CreateProjectDto>
+    {
+        public ProjectDtoValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Task name is required")
+                .MaximumLength(100).WithMessage("Name must be less than 100 characters");
+
+            RuleFor(x => x.Description)
+                .MaximumLength(500).WithMessage("Description must be less than 500 characters");
+        }
+    }
+}
---
+++ api/Taskify.Api/Validators/TaskDtoValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using Taskify.Api.Dtos;
+
+namespace Taskify.Api.Validators
+{
+    public class TaskDtoValidator : AbstractValidator<TaskDto>
+    {
+        public TaskDtoValidator()
+        {
+            RuleFor(x => x.Title)
+                .NotEmpty().WithMessage("Task title is required")
+                .MaximumLength(100).WithMessage("Title must be less than 100 characters");
+
+            RuleFor(x => x.Description)
+                .MaximumLength(500).WithMessage("Description must be less than 500 characters");
+        }
+    }
+}
